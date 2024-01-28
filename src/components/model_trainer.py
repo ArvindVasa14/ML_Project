3fb7a8eaaa9fba0cca4891bd86295fae32b30ce7
arvindvasa1122@gmail.com <arvindaiml14@gmail.com>
2024-01-28 15:56:46 +0530

    Model Trainer

---
+++ src/components/model_trainer.py
@@ -0,0 +1,89 @@
+import os
+import sys
+import numpy as np 
+import pandas as pd 
+from src.exception import CustomException
+from src.logger import logging
+
+# Models
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVR
+from sklearn.linear_model import LinearRegression
+
+# Ensemble models
+from xgboost import XGBRegressor
+from catboost import CatBoostRegressor
+from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor
+
+# Regularization models
+from sklearn.linear_model import Ridge, Lasso
+
+# Hyperparameter tuning
+from sklearn.model_selection import RandomizedSearchCV
+
+from src.utils import evaluate_models, save_object
+# Model Evaluation 
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from dataclasses import dataclass
+
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path= os.path.join("artifact","model.pkl")
+
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config= ModelTrainerConfig()
+
+    def initate_model_trainer(self, train_arr, test_arr):
+        try:
+            logging.info("spliting training training and test input data")
+            x_train, y_train, x_test, y_test= [
+                train_arr[:,:-1],
+                train_arr[:,-1],
+                test_arr[:,:-1],
+                test_arr[:,-1]
+            ]
+
+            models= {
+                "LinearRegression": LinearRegression(),
+                "Lasso": Lasso(),
+                "Ridge": Ridge(),
+                "K-Neighbors Regressor" :KNeighborsRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Random Forest Regressor": RandomForestRegressor(),
+                "XGBRegressor": XGBRegressor(),
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor()
+            }
+
+            model_report: dict= evaluate_models(x_train= x_train, y_train= y_train, x_test= x_test,
+                                                y_test= y_test, models= models)
+            highest_r_squared= 0
+            best_model:str= ''
+            for model_name, report in model_report.items():
+                if report['r2_score_test']>highest_r_squared:
+                    highest_r_squared= report['r2_score_test']
+                    best_model= model_name
+
+            if highest_r_squared< 0.6:
+                raise CustomException("No best model found")
+                logging.info("No best model found")
+            
+            bestModel= models[best_model].fit(x_train, y_train)
+
+            save_object(
+                file_path= self.model_trainer_config.trained_model_file_path,
+                obj= bestModel
+            )
+
+            predicted= bestModel.predict(x_test)
+
+            return r2_score(y_test, predicted)
+        
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+
+
--- src/utils.py
+++ src/utils.py
@@ -9,6 +9,8 @@ from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
+# Model Evaluation 
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 def save_object(file_path, obj):
     try:
@@ -21,3 +23,34 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+def evaluate_models(x_train, y_train, x_test, y_test, models):
+    model_performance_report= {}
+    for model_name, model in models.items():
+        # training
+        model.fit(x_train, y_train)
+
+        # prediction
+        y_train_pred= model.predict(x_train)
+        y_test_pred= model.predict(x_test)
+
+        # Evaluate model
+        mae_train, mse_train, r2_square_train= evaluate_model(y_train, y_train_pred)
+        mae_test, mse_test, r2_square_test= evaluate_model(y_test, y_test_pred)
+        
+        model_performance_report[model_name]= {
+            'mae_train': mae_train,
+            'mse_train': mse_train,
+            'r2_score_train': r2_square_train * 100,
+            'mae_test': mae_test,
+            'mse_test': mse_test,
+            'r2_score_test': r2_square_test * 100
+        }
+    return model_performance_report
+
+def evaluate_model(actual, predicted):
+    mae= mean_absolute_error(actual, predicted)
+    mse= mean_squared_error(actual, predicted)
+    rmse= np.sqrt(mse)
+    r2_square= r2_score(actual, predicted)
+    return mae, rmse, r2_square
